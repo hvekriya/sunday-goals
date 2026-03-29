47531deadbe487320ce282b630ff1b100f55a150
Haresh Vekriya <haresh.vekriya@gmail.com>
2026-03-29 22:48:39 +0100

    Moving to vercel

---
+++ index.mjs
@@ -0,0 +1,7 @@
+/**
+ * Vercel entry: default-exported Express app (see https://vercel.com/guides/using-express-with-vercel).
+ * Local dev: use `npm run dev` / `node server/index.js`.
+ */
+import app from './server/app.js';
+
+export default app;
